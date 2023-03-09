0c798a03cc895d152aca2379c2a4977ac86e0a06
User DontHaveAName <naveh.racovsky@gmail.com>
2023-03-09 22:05:31 +0200

    Deployed the file using flask server

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+
+@app.route("/")
+def home():
+    return render_template('game.html')
+
+if __name__ == "__main__":
+    app.run(ssl_context='adhoc')
